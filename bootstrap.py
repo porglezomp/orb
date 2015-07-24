f2750612c9a97ff1524f7bc102a748ad6ad7e4d8
Caleb Jones <bionicle.dragon@gmail.com>
2015-07-24 00:03:59 -0400

    Make run command work
    Preserve shebang line in processing
    Simplify bootstrap script
    Improve formatting in orb output

--- bootstrap.py
+++ bootstrap.py
@@ -1,23 +1,18 @@
+import subprocess
+
 in_code = False
 code = []
 with open('orb.lpy', 'r') as f:
     for line in f:
         if line.strip()[:3] == '```':
-            # toggle between in and out of code
+            # Toggle between in and out of code
             in_code = not in_code
-            if line.lstrip()[3:].isspace():
-                code.append('# ###\n')
-            else:
-                code.append('# ### ' + line.lstrip()[3:])
             continue
         
         if in_code:
-            code.append(line.rstrip() + '\n')
-        else:
-            if line.isspace():
-                code.append('#\n')
-            else:
-                code.append('# ' + line)
+            code.append(line)
 
 with open('orb.py', 'w') as f:
       f.write(''.join(code))
+
+subprocess.Popen(['python', 'orb.py', 'compile', 'orb.lpy']).wait()
